dc3b24a6671f4d9a2b7c1570e34881ea80a95e04
Yash Vishnoi <yashvishnoi007@gmail.com>
2023-02-20 14:25:14 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxQuIjZb-nnK4RWieqdLuMA56uDStPRnM",
+    authDomain: "my-crypto-77d4b.firebaseapp.com",
+    projectId: "my-crypto-77d4b",
+    storageBucket: "my-crypto-77d4b.appspot.com",
+    messagingSenderId: "502257480614",
+    appId: "1:502257480614:web:5fac561267275276765567"
+  };
+
+  export default firebaseConfig;
